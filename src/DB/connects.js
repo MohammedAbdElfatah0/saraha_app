f1ceda3efc63497e001f365f46886b23dbbca24c
Mohammed Mohammed Abd El-Fatah <hamoabdelfatah611@gmail.com>
2025-08-09 11:07:32 +0300

    review same problem

--- src/DB/connects.js
+++ src/DB/connects.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 export function connectDB() {
-    mongoose.connect("mongodb://localhost:27017/saraha_app",)
+    mongoose.connect("mongodb://127.0.0.1:27017/saraha_app",)
         .then(() => console.log("MongoDB connected"))
         .catch(err => console.error("MongoDB connection error:", err));
 }
