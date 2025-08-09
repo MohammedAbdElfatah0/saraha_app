6a8ffae8f2b88ab8d7f6f1a053882439baea2578
Mohammed Mohammed Abd El-Fatah <hamoabdelfatah611@gmail.com>
2025-08-09 11:04:09 +0300

    create user model

--- src/DB/models/user.model.js
+++ src/DB/models/user.model.js
@@ -1,6 +1,59 @@
-// import { model, Schema } from "mongoose";
+import { model, Schema } from "mongoose";
 
-// const userSchema=new Schema({},{
-//     timestamps: true,
-// })
-// const user=model("User", );
+const userSchema = new Schema({
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    phoneNumber: {
+        type: String,
+        required: true,
+        unique: true,
+        // trim: true,
+    },
+    dob: {
+        type: Date,
+    }
+
+
+}, 
+{
+    timestamps: true,
+})
+
+userSchema.virtual("fullName",).get(function () {
+    return `${this.firstName} ${this.lastName}`;
+});
+userSchema.virtual("fullname").set(function (value) {
+    //  [this.firstName, this.lastName] = value.split(" ");
+    const [firstName, lastName] = value.split(" ");
+    this.firstName = firstName;
+    this.lastName = lastName;
+});
+
+
+userSchema.virtual("age").get(function () {
+    //new Date().getFullYear() - new Data(this.dob).getFullYear();
+    return new Date().getFullYear() - this.dob.getFullYear();
+});
+
+export const user = model("User", userSchema);
