01c037a7d4fd2b04af9087898331a566f1bf85d4
Mohammed Mohammed Abd El-Fatah <hamoabdelfatah611@gmail.com>
2025-12-14 11:02:43 +0200

    fix:deploy

--- src/utils/security/encrypt.js
+++ src/utils/security/encrypt.js
@@ -1,6 +1,7 @@
 import CryptoJS from "crypto-js";
 
 const key = process.env.SECRET_KEY ;
+console.log(key[0],key[2])//_@
 export function encryptData(data) {
     return CryptoJS.AES.encrypt(data,key).toString();// Encrypt the data
 
