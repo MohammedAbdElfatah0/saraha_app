35b730f0138874468d8c435c479e87b759474f62
Mohammed Mohammed Abd El-Fatah <hamoabdelfatah611@gmail.com>
2025-12-14 11:05:48 +0200

    fix:create otp as string

--- src/utils/otp/index.js
+++ src/utils/otp/index.js
@@ -6,7 +6,7 @@
 
 
 export function generateOtp(expireTime = 2 * 60 * 1000) {
-    const otp = Math.floor(Math.random() * 90000 + 10000);
+    const otp = Math.floor(Math.random() * 90000 + 10000).toString();
     const otpExpiration = Date.now() + expireTime;
     return { otp, otpExpiration };
 } 
