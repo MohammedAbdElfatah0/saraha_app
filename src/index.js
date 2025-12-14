b963e9104e70042820e684c98edbb4c86f386be7
Mohammed Mohammed Abd El-Fatah <hamoabdelfatah611@gmail.com>
2025-12-14 10:39:44 +0200

    read env

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import dotenv from "dotenv";
-import path from "path";
-dotenv.config({ path: path.resolve(".env") })
+// import dotenv from "dotenv";
+// import path from "path";
+// dotenv.config({ path: path.resolve(".env") })
 
 import express from 'express';
 import { bootstrap } from './app.controller.js';
